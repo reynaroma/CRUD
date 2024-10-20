7f5e46ad551517f017f170fa08e94573246d6f0c
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-20 17:30:11 -0600

    Destructure the method property fromreq object then call dbConnect to connect to database.

--- book-collection/pages/api/books/[id].js
+++ book-collection/pages/api/books/[id].js
@@ -2,5 +2,7 @@ import dbConnect from "../../../lib/dbConnect";
 import Book from "../../../models/Book";
 
 export default async function handler(req, res) {
+  const { method } = req;
 
+  await dbConnect();
 }
