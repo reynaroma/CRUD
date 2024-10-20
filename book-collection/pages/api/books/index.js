4e0c994bc65790469f9be731da0db47a64fc47ac
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-20 15:30:48 -0600

    Create a book based on the req. body then return a 201 status if success, and if there's an error send a 400 status

--- book-collection/pages/api/books/index.js
+++ book-collection/pages/api/books/index.js
@@ -16,6 +16,13 @@ export default async function handler(req, res) {
       }
       break;
     case "POST":
+      try {
+        const book = await Book.create(req.body);
+        res.status(201).json({ success: true, data: book})
+      } catch (error) {
+        res.status(400).json({ success: false, error });
+      }
+      break;
     default:
   }
 }
