60c71d78e371cf83921dc31e41d38be4a1f6fc9b
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-19 18:41:40 -0600

    Setup database connection and check if the cached is defined

--- book-collection/lib/dbConnect.js
+++ book-collection/lib/dbConnect.js
@@ -9,3 +9,24 @@ if(!MONGODB_URL) { // Check if MONGODB_URL is defined
 };
 
 let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null}
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => {
+      return mongoose;
+    })
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+export default dbConnect;e
