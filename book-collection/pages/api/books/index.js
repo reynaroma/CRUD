e4663a9097594b29afb084a6ce25f0a2a67ed14c
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-20 15:24:10 -0600

    Create a switch condition for GET and POST

--- book-collection/pages/api/books/index.js
+++ book-collection/pages/api/books/index.js
@@ -5,4 +5,17 @@ export default async function handler(req, res) {
   const { method } = req;
 
   await dbConnect();
+
+  switch (method) {
+    case "GET":
+      try {
+        const books = await Book.find();
+        res.status(200).json({ success: true, data: books });
+      } catch (error) {
+        res.status(400).json({ success: false, error });
+      }
+      break;
+    case "POST":
+    default:
+  }
 }
