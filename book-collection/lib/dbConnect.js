adcf701c5c8422829e57e84c7b05bf55c66bf8dd
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-19 18:35:29 -0600

    Write a code to check if the MONGODB_URL is defined

--- book-collection/lib/dbConnect.js
+++ book-collection/lib/dbConnect.js
@@ -2,3 +2,9 @@ import mongoose from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
+if(!MONGODB_URL) { // Check if MONGODB_URL is defined
+  throw new Error(
+    'Please define the MONGODB_URL environment variable inside .env.local'
+  )
+};
+
