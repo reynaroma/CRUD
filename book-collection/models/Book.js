c1c73c78ee6db065faee72ced2d14200eb4e6217
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-19 18:46:27 -0600

    Create a new folder for models and create a Book Model

---
+++ book-collection/models/Book.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please provide the book title.'],
+  },
+  author: {
+    type: String,
+    required: [true, 'Please provide the author of the book.'],
+  },
+  description: {
+    type: String,
+  }
+});
+
+export default mongoose.model("Book", BookSchema);
