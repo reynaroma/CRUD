12e09f3422c213617b2ce84f9290fe213982fc45
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-28 14:57:51 -0600

    Set data to the state variable to update the books

--- book-collection/app/page.tsx
+++ book-collection/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
   // create a function to call the api routes
   const fetchBooks = async () => {
     const response = await axios.get("/api/books");
+    setBooks(response.data.data);
   }
   return (
 
